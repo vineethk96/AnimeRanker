9a3ad9aaa665d00559a526601283db1b68e07d30
Vineeth Kirandumkara <vineethk@vt.edu>
2020-09-26 23:02:32 -0400

    Comments added for the models

--- anime-ranker/backend/models/reviews.model.js
+++ anime-ranker/backend/models/reviews.model.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    animeName: { type: String, required: true, unique: true, trim: true, minlength: 3 },
-    rating: { type: Number, required: true },
-    description: { type: String, required: true },
+    reviewID: { type: Number, required: true, unique: true, minlength: 5, maxlength: 5},            // Unique reviewIDs. This will be in the users model
+    animeName: { type: String, required: true, unique: true, trim: true, minlength: 3 },            // Anime name
+    rating: { type: Number, required: true },                                                       // Rating out of 5
+    description: { type: String },                                                                  // Text description of the anime, why you liked it.
 }, {
   timestamps: true,
 });
--- anime-ranker/backend/models/users.model.js
+++ anime-ranker/backend/models/users.model.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: { type: String, required: true, unique: true, trim: true, minlength: 3 },
-    watchList: { type: Array },
-    friendsList: { type: Array },
-    reviews: { type: Array }
+    username: { type: String, required: true, unique: true, trim: true, minlength: 3 },         // Unique Usernames that the users create
+    watchList: { type: Array },                                                                 // Array of anime that they want to watch
+    friendsList: { type: Array },                                                               // Array of friends
+    reviews: { type: Array }                                                                    // Array of Unique reviewIDs
 }, {
   timestamps: true,
 });
